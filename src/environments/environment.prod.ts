34735d0205bf0bbe514c58695dfc1dbe130e5271
alex <changli@steelkiwi.com>
2017-06-12 21:50:32 +0300

    Dummy api connection data (dev and prod)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+// TODO: set prod env values before building prod bundle
 export const environment = {
-  production: true
+  production: true,
+  gigyaApiConfig: {
+    apiUrl: 'TODO',
+    userKey: 'TODO',
+    userSecret: 'TODO',
+    apiKey: 'TODO'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,13 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// TODO: set dev env values to work with app in dev mode locally
 export const environment = {
-  production: false
+  production: false,
+  gigyaApiConfig: {
+    apiUrl: 'TODO',
+    userKey: 'TODO',
+    userSecret: 'TODO',
+    apiKey: 'TODO'
+  }
 };
