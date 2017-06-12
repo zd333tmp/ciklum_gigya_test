e06c09ba86d58eb614ac05688c45d2a19bc41b4d
alex <changli@steelkiwi.com>
2017-06-12 22:22:06 +0300

    Account options module stub, account options model.

---
+++ src/app/account-options/account-options.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * Account options feature module.
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AccountOptionsModule { }
---
+++ src/app/account-options/shared/account-options.model.ts
@@ -0,0 +1,58 @@
+/**
+ * Account options model.
+ * Constructor takes raw object (received from back-end API) and saves all fields to private properties.
+ * All properties are exposed via getters to ensure immutability.
+ * TODO: getters are not best choice (a lot of boilerplate code), consider refactoring with `immutable.js`
+ */
+export class AccountOptions {
+  private _verifyEmail: boolean;
+  private _verifyProviderEmail: boolean;
+  private _allowUnverifiedLogin: boolean;
+  private _preventLoginIDHarvesting: boolean;
+  private _sendWelcomeEmail: boolean;
+  private _sendAccountDeletedEmail: boolean;
+  private _defaultLanguage: string;
+  private _loginIdentifierConflict: string;
+  private _loginIdentifiers: string;
+
+  constructor(rawAccountOptions: any) {
+    // TODO: take more care of validating and checking raw object
+    this._verifyEmail = rawAccountOptions.verifyEmail;
+    this._verifyProviderEmail = rawAccountOptions.verifyProviderEmail;
+    this._allowUnverifiedLogin = rawAccountOptions.allowUnverifiedLogin;
+    this._preventLoginIDHarvesting = rawAccountOptions.preventLoginIDHarvesting;
+    this._sendWelcomeEmail = rawAccountOptions.sendWelcomeEmail;
+    this._sendAccountDeletedEmail = rawAccountOptions.sendAccountDeletedEmail;
+    this._defaultLanguage = rawAccountOptions.defaultLanguage;
+    this._loginIdentifierConflict = rawAccountOptions.loginIdentifierConflict;
+    this._loginIdentifiers = rawAccountOptions.loginIdentifiers;
+  }
+
+  get verifyEmail() {
+    return this._verifyEmail;
+  }
+  get verifyProviderEmail() {
+    return this._verifyProviderEmail;
+  }
+  get allowUnverifiedLogin() {
+    return this._allowUnverifiedLogin;
+  }
+  get preventLoginIDHarvesting() {
+    return this._preventLoginIDHarvesting;
+  }
+  get sendWelcomeEmail() {
+    return this._sendWelcomeEmail;
+  }
+  get sendAccountDeletedEmail() {
+    return this._sendAccountDeletedEmail;
+  }
+  get defaultLanguage() {
+    return this._defaultLanguage;
+  }
+  get loginIdentifierConflict() {
+    return this._loginIdentifierConflict;
+  }
+  get loginIdentifiers() {
+    return this._loginIdentifiers;
+  }
+}
