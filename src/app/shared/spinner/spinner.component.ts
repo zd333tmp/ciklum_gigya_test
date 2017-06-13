333efbe784f9919d9b1284b264740d6872026738
alex <changli@steelkiwi.com>
2017-06-14 01:49:29 +0300

    App bundle and compodoc static assets

--- src/app/shared/spinner/spinner.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'gig-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.styl'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class SpinnerComponent { }
